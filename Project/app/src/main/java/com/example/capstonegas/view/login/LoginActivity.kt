928a90247f66aa04f503afa12aeef90d46359e75
Ahdan Amanullah <ahdanamanullah@gmail.com>
2022-05-16 20:31:01 +0700

    feat: init layout

---
+++ Project/app/src/main/java/com/example/capstonegas/view/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.capstonegas.view.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.capstonegas.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
