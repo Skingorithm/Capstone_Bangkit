f70d65e8684d8edf9727e3235418bcea53cd24a9
MHamidA <99332005+MHamidA@users.noreply.github.com>
2022-05-30 13:07:58 +0700

    Delete Project directory

--- Project/app/src/main/java/com/example/capstonegas/model/UserModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.capstonegas.model
-
-data class UserModel(
-    val id: String,
-    val token: String,
-    val isLogin: Boolean
-)
--- Project/app/src/main/java/com/example/capstonegas/viewmodel/LoginViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.capstonegas.viewmodel
-
-class LoginViewModel {
-}
