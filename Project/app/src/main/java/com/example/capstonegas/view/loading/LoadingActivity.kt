587b6801edef05ade88a5612fad7a9ddb2a530a9
husinassegaff <husinassegaff15@gmail.com>
2022-06-02 21:39:42 +0700

    feat: add loading & failed layout

---
+++ Project/app/src/main/java/com/example/capstonegas/view/loading/LoadingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.capstonegas.view.loading
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.capstonegas.R
+
+class LoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+    }
+}
