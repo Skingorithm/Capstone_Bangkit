4b7ca9868394a610de3e7efd044d68ed512a6663
husinassegaff <husinassegaff15@gmail.com>
2022-06-02 20:49:07 +0700

    feat: add add routine layout

---
+++ Project/app/src/main/java/com/example/capstonegas/view/routine/AddRoutineActivity.kt
@@ -0,0 +1,12 @@
+package com.example.capstonegas.view.routine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.capstonegas.R
+
+class AddRoutineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_routine)
+    }
+}
