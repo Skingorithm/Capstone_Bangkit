97be9bb827c121ee26ca5fba61cac50a9c94cb66
husinassegaff <husinassegaff15@gmail.com>
2022-06-12 00:17:02 +0700

    feat: add activity detail history

---
+++ Project/app/src/main/java/com/example/capstonegas/view/detailhistory/DetailHistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.capstonegas.view.detailhistory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.capstonegas.R
+
+class DetailHistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_history)
+    }
+}
