bf2018c9388ae1ebe08790f9b930175e03382d2a
Ahdan Amanullah <ahdanamanullah@gmail.com>
2022-06-13 05:51:48 +0700

    fix: resolve large base64 size logic

--- Project/app/src/main/java/com/example/capstonegas/view/loading/LoadingActivity.kt
+++ Project/app/src/main/java/com/example/capstonegas/view/loading/LoadingActivity.kt
@@ -1,25 +1,31 @@
 package com.example.capstonegas.view.loading
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import com.example.capstonegas.databinding.ActivityLoadingBinding
 import com.example.capstonegas.model.ResultData
 import com.example.capstonegas.view.camera.UploadActivity
 import com.example.capstonegas.view.resultskincare.ResultSkincareActivity
 import com.example.capstonegas.viewmodel.LoadingViewModel
+import java.io.File
 
 class LoadingActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoadingBinding
     private val viewModel: LoadingViewModel by viewModels()
+    private lateinit var base64: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoadingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val base64 = intent.getStringExtra("image").toString()
+        //val base64 = intent.getStringExtra("image").toString()
+        //function to open base64 file
+        openFile()
 
         viewModel.postImage(base64)
 
@@ -64,6 +70,15 @@ class LoadingActivity : AppCompatActivity() {
         }
 
         setupAction()
+    }
+
+    private fun openFile(){
+        val path = applicationContext.filesDir
+        val letDirectory = File(path, "Picture Base64")
+        val file = File(letDirectory, "fotoku.txt")
+        base64 = file.readText()
+        Log.d("base64", base64)
+//        Log.d("File", "File path: $file")
     }
 
     private fun setupAction() {
